54c3d1413ace0ea2ed997e38174e8e8fd8e59d8f
levikantor <55114378+levikantor@users.noreply.github.com>
2021-09-28 12:07:32 +0200

    Add files via upload

--- mindconnectiotextension_esp/mqtt_esp/mqtt_esp.ino
+++ mindconnectiotextension_esp/mqtt_esp/mqtt_esp.ino
@@ -13,66 +13,32 @@ DHT dht(DHTPin, DHTTYPE);
 String Temperature;
 String Humidity;
 
-const char *ssid = "i40tk"; 
-const char *password = "PbKbTtKa5";
+const char* ssid = "i40tk";
+const char* password = "PbKbTtKa5"; 
 
-const char *mqtt_broker = "mciotextension.eu1.mindsphere.io";
-const char *topic = "s/us";
-const char *mqtt_username = "bmedev/mindconnectuser";
-const char *mqtt_password = "Kiskacsa0";
-//set mqtt clientID
-const char *clientid="2e04f278-8e04-48a5-821e-69f26448af38";
-
-const int mqtt_port = 1883;
-
-IPAddress local_IP(192, 168, 33, 150);
-// Set your Gateway IP address
-IPAddress gateway(192, 168, 33, 254);
-IPAddress subnet(255, 255, 255, 0);
+const char* mqtt_Server = "mciotextension.eu1.mindsphere.io";
+const int mqtt_Port = 1883;
+const char* mqtt_user = "bmedev/mindconnectuser";
+const char* mqtt_password = "Kiskacsa0";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void setup() {
- // Set software serial baud to 115200;
- Serial.begin(115200);
- Serial.flush();
+/*void callback(char* topic, byte* payload, unsigned int length) {
 
- pinMode(DHTPin, INPUT);
- dht.begin();
- // connecting to a WiFi network
- if (!WiFi.config(local_IP, gateway, subnet)) {
-    Serial.println("STA Failed to configure");
- }
- WiFi.begin(ssid, password);
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
 
- while (WiFi.status() != WL_CONNECTED) {
-     delay(500);
-     Serial.println("Connecting to WiFi..");
- }
- Serial.println("Connected to the WiFi network");
- //connecting to a mqtt broker
- client.setServer(mqtt_broker, mqtt_port);
-
- while (!client.connected()) {
-     Serial.println("Connecting to public mqtt broker.....");
-     if (client.connect(clientid, mqtt_username, mqtt_password)) {
-         Serial.println("Public mqtt broker connected");
-     } else {
-         Serial.print("failed with state ");
-         Serial.print(client.state());
-         delay(2000);
-     }
- }
-}
-
-void reconnect(){
-  while (!client.connected()) {
-    if (client.connect(clientid, mqtt_username, mqtt_password)) {
-           Serial.println("Public mqtt broker connected");
-    }
-    Serial.println("Working on it");
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
   }
+
+  Serial.println();
+  Serial.println("-----------------------");
+
+}*/
+void sendMessage(){
   Temperature = String(dht.readTemperature()); // Gets the values of the temperature
   Humidity = String(dht.readHumidity()); // Gets the values of the humidity
   String temp="200,measurement,Temperature,"+Temperature+"\n"+"200,measurement,Humidity,"+Humidity;
@@ -80,12 +46,50 @@ void reconnect(){
   temp.toCharArray(payload,temp.length()+1);
   Serial.println("Temp:"+Temperature);
   Serial.println("Humi:"+Humidity);
-  client.publish(topic, payload);
-  client.subscribe(topic);
+  client.publish("s/us", payload);
   delay(1000);
 }
 
-void loop() {
-  reconnect();
-  client.loop();
+void reconnect() {
+  // Loop until reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "2e04f278-8e04-48a5-821e-69f26448af38";
+    // Attempt to connect
+    if (client.connect(clientId.c_str(), mqtt_user, mqtt_password)) {
+    Serial.println("connected");
+    client.subscribe("s/us");
+    } else {
+      Serial.print("failed, rc= ");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+  sendMessage();
 }
+
+
+void setup() {
+
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to WiFi network");
+
+  client.setServer(mqtt_Server, mqtt_Port);
+  //client.setCallback(callback);
+  reconnect();
+  //client.subscribe("s/us"); //I also tryed s/ds
+
+}
+
+void loop() {
+  client.loop();
+  delay(1000);
+  reconnect();
+} 
